47c3ef242fea2df59903da3b3ed8b89b8a980b6b
kst2221 <kst2221@naver.com>
2025-06-01 14:57:20 +0900

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,15 @@
-# keep_alive.py
 from flask import Flask
-import threading
+from threading import Thread
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "I'm alive!", 200
+    return "✅ I'm alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = threading.Thread(target=run)
+    t = Thread(target=run)
     t.start()
